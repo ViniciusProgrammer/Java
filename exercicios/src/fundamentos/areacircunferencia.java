831a3e2e2555708e02146d89baf225d0be10463f
Vinicius Davi <viniprogrammer@hotmail.com>
2024-01-22 15:13:56 -0300

    Exercicio da area de circunferencia resolvido, constante e variável

---
+++ exercicios/src/fundamentos/areacircunferencia.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class areacircunferencia {
+    public static void main(String[] args){
+        double raio = 3.4;
+        //Definição de constante
+        final double PI = 3.14159;
+        double area = PI * raio * raio;
+
+        System.out.println("Area = " + area);
+        System.out.println(area);
+    }
+}
