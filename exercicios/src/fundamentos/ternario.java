21246b1f4fa23056b5a8f85940578c6d4d9fc3b1
vinicius davi <viniprogrammer@hotmail.com>
2024-01-29 12:35:53 -0300

    Operador ternário

---
+++ exercicios/src/fundamentos/ternario.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class ternario {
+    public static void main(String[] args){
+        double media = 7.6;
+        String resultado = media >= 7.0? "APROVADO" : "REPROVADO";
+
+        System.out.println("O alnuno(a) está " + resultado);
+    }
+}
