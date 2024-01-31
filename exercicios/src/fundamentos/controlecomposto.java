6106903986194124f6046a1493fb348ce3eaec92
vinicius davi <viniprogrammer@hotmail.com>
2024-01-31 13:10:52 -0300

    Adicionando o Locale.setDefault(Locale.US) para immpriir corretamente no terminal valores com ponto flutuante

--- exercicios/src/fundamentos/controlecomposto.java
+++ exercicios/src/fundamentos/controlecomposto.java
@@ -1,9 +1,11 @@
 package fundamentos;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class controlecomposto{
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Digite a nota: ");
