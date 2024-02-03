81bf7c6ebe6a33a569edf573642faae21df3f324
vinicius davi <viniprogrammer@hotmail.com>
2024-02-03 13:17:11 -0300

    Update na classe

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -4,4 +4,21 @@ public class Data {
     int dia;
     int mes;
     int ano;
+
+    Data(){
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data(int diaInicial, int mesInicial, int anoInicial){
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+
+    String dataFormatada(){
+        String data = dia + "/" + mes + "/" + ano;
+        return data;
+    }
 }
