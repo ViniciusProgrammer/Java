253d9cff2cb3bb5f115d83323f1568beb1ac4b9e
vinicius davi <viniprogrammer@hotmail.com>
2024-02-05 14:39:22 -0300

    Uso do this

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -1,20 +1,23 @@
 package classe;
 
 public class Data {
-    int dia;
-    int mes;
-    int ano;
+    int dia; //obj
+    int mes; //obj
+    int ano; //obj
 
     Data(){
         dia = 1;
         mes = 1;
         ano = 1970;
+
+        //this(1, 1, 1970); Uma forma de chamar um construtor dentro de outro
     }
 
-    Data(int diaInicial, int mesInicial, int anoInicial){
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+    Data(int dia, int mes, int ano){
+    //this.objeto = parametro (esse é o molde que o this usa)
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     String dataFormatada(){
