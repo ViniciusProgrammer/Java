7764f5aa3a720e8adfbe24456c1468e69dda307a
vinicius davi <viniprogrammer@hotmail.com>
2024-02-05 14:40:11 -0300

    Utilizando os diferente construtores criados

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -1,27 +1,14 @@
 package classe;
 
-import java.util.Scanner;
-
 public class DataTeste {
     public static void main(String[] args){
-        Data data1 = new Data();
+        Data data1 = new Data(31, 12, 2020);
         Data data2 = new Data();
 
-        Scanner entrada = new Scanner(System.in);
+        System.out.println("Utilizando o contrutor com parametrizado");
+        System.out.println(data1.dataFormatada());
 
-        System.out.print("Digite a primeira data: ");
-        data1.dia = entrada.nextInt();
-        data1.mes = entrada.nextInt();
-        data1.ano = entrada.nextInt();
-
-        System.out.print("Digite a segunda data: ");
-        data2.dia = entrada.nextInt();
-        data2.mes = entrada.nextInt();
-        data2.ano = entrada.nextInt();
-
-        System.out.println("Data 1: " + data1.dia + "/" + data1.mes + "/" + data1.ano);
-        System.out.println("Data 2: " + data2.dia + "/" + data2.mes + "/" + data2.ano);
-
-        entrada.close();
+        System.out.println("Utilizando o contrutor padrão");
+        System.out.println(data2.dataFormatada());
     }
 }
