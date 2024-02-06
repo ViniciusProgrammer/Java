32605b7726300128efbe35098bd9396013ffda38
vinicius davi <viniprogrammer@hotmail.com>
2024-02-06 15:14:59 -0300

    Iplementei a classe e fiz o exercicio

--- exercicios/src/classe/Jantar.java
+++ exercicios/src/classe/Jantar.java
@@ -2,5 +2,15 @@ package classe;
 
 public class Jantar {
     public static void main(String[] args){
-}
+        Comida c1 = new Comida("Arroz", 0.265);
+        Comida c2 = new Comida("Feijão", 0.442);
+
+        Pessoa p1 = new Pessoa("Ana", 55.5);
+        
+        System.out.println(p1.apresentar());
+        p1.comer(c1);
+        System.out.println(p1.apresentar());
+        p1.comer(c2);
+        System.out.println(p1.apresentar());
+    }
 }
--- exercicios/src/classe/Pessoa.java
+++ exercicios/src/classe/Pessoa.java
@@ -1,5 +1,21 @@
 package classe;
 
 public class Pessoa {
+    String nome;
+    double peso;
 
+    Pessoa(String nome, double peso){
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    void comer(Comida comida){
+        if(comida != null){
+            this.peso += comida.peso;
+        }
+    }
+
+    String apresentar(){
+        return "Olá eu me chamo " + nome + " e tenho " + peso + " Kg";
+    }
 }
